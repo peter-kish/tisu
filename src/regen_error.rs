6d0e161b3d8296320b99501b6e2605332b14918a
Peter Kis <pkish023@gmail.com>
2025-05-11 13:08:21 +0200

    Missing error variants

--- src/regen_error.rs
+++ src/regen_error.rs
@@ -1,14 +1,16 @@
 use std::{error::Error, fmt::Display};
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub enum RegenError {
     OutOfBounds,
+    InvalidArgument,
 }
 
 impl Display for RegenError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             RegenError::OutOfBounds => write!(f, "Out of map bounds"),
+            RegenError::InvalidArgument => write!(f, "Invalid argument"),
         }
     }
 }
