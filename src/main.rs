54046929996680321676c7989a35575c2d97860b
Peter Kis <pkish023@gmail.com>
2025-05-15 22:49:26 +0200

    Expanding main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,66 @@ mod rect2_utils;
 mod regen_error;
 mod vector2;
 
-fn main() {
-    let mut map = Map::<char>::new(Vector2u::new(10, 10));
-    map.fill('a');
-    let r = map
-        .border_rect((0, 0, 10, 10).try_into().unwrap(), 'b')
-        .expect("border_rect failed")
-        .unwrap();
-    map.h_line_rect(r, 2, 'c').expect("h_line_rect failed");
-    let (r1, _) = map.v_line_rect(r, 2, 'd').expect("v_line_rect failed");
-    map.fill_rect(r1.unwrap(), 'e').expect("fill_rect failed");
+use clap::Parser;
+use image::{Rgb, RgbImage};
 
-    map.print();
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct CmdLineArgs {
+    /// Output file path
+    #[arg(short, long, default_value_t = String::from("output.png"))]
+    output: String,
+}
+
+#[derive(Clone, Default)]
+enum MapTile {
+    #[default]
+    Concrete,
+    Asphalt,
+    Grass,
+    Hedge,
+    Wall,
+}
+
+impl From<&MapTile> for Rgb<u8> {
+    fn from(value: &MapTile) -> Self {
+        match value {
+            MapTile::Concrete => Rgb([128, 128, 128]),
+            MapTile::Asphalt => Rgb([0, 0, 0]),
+            MapTile::Grass => Rgb([0, 255, 0]),
+            MapTile::Hedge => Rgb([0, 128, 0]),
+            MapTile::Wall => Rgb([255, 128, 0]),
+        }
+    }
+}
+
+fn generate_map() -> Map<MapTile> {
+    let mut map = Map::<MapTile>::new(Vector2u::new(20, 20));
+    map.v_line(10, MapTile::Asphalt).expect("v_line_failed");
+    map
+}
+
+fn draw_map(map: Map<MapTile>) -> RgbImage {
+    let mut image = RgbImage::new(
+        map.get_size().x.try_into().unwrap(),
+        map.get_size().y.try_into().unwrap(),
+    );
+    for x in 0..map.get_size().x {
+        for y in 0..map.get_size().y {
+            image.put_pixel(
+                x.try_into().unwrap(),
+                y.try_into().unwrap(),
+                map.get((x, y).into()).unwrap().into(),
+            );
+        }
+    }
+    image
+}
+
+fn main() {
+    let args = CmdLineArgs::parse();
+    let map = generate_map();
+    draw_map(map)
+        .save(args.output)
+        .expect("Failed to save image");
 }
