4b38373065fde26cbf85360ee176371099d71724
Peter Kis <pkish023@gmail.com>
2025-05-25 13:48:29 +0200

    TiledMapLoader: Fix parsing bug

---
+++ data/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="default" tilewidth="16" tileheight="16" tilecount="6" columns="2">
+ <image source="tileset.png" width="32" height="48"/>
+</tileset>
--- src/tiled_map_loader.rs
+++ src/tiled_map_loader.rs
@@ -5,7 +5,7 @@ use tiled::Loader;
 pub struct TiledMapLoader {}
 
 impl TiledMapLoader {
-    pub fn load(file: &str) -> Result<Map<u32>, RegenError> {
+    pub fn load(file: &str) -> Result<Map<Option<u32>>, RegenError> {
         let mut loader = Loader::new();
         let tmx_map = loader
             .load_tmx_map(file)
@@ -20,13 +20,14 @@ impl TiledMapLoader {
         let tile_layer = tile_layers.next().ok_or(RegenError::InvalidArgument)?;
 
         if let tiled::TileLayer::Finite(finite) = tile_layer {
-            let mut result = Map::<u32>::new((finite.width(), finite.height()).into());
+            let mut result = Map::<Option<u32>>::new((finite.width(), finite.height()).into());
             for x in 0..finite.width() {
                 for y in 0..finite.height() {
-                    let tile = finite
-                        .get_tile(x as i32, y as i32)
-                        .ok_or(RegenError::InvalidArgument)?;
-                    result.set((x, y).into(), tile.id())?;
+                    if let Some(tile) = finite.get_tile(x as i32, y as i32) {
+                        result.set((x, y).into(), Some(tile.id()))?;
+                    } else {
+                        result.set((x, y).into(), None)?;
+                    }
                 }
             }
             Ok(result)
@@ -42,12 +43,14 @@ mod tests {
 
     #[test]
     fn test_load() {
-        let result = TiledMapLoader::load("/home/peter/tilemap.tmx");
+        let result = TiledMapLoader::load(
+            format!("{}/{}", env!("CARGO_MANIFEST_DIR"), "data/3x3.tmx").as_str(),
+        );
 
         assert!(result.is_ok());
         let map = result.unwrap();
-        assert_eq!(map.get_size(), (10, 10).into());
-        assert_eq!(map.get((0, 0).into()).unwrap(), &0);
-        assert_eq!(map.get((1, 1).into()).unwrap(), &1);
+        assert_eq!(map.get_size(), (3, 3).into());
+        assert_eq!(map.get((0, 0).into()).unwrap(), &None);
+        assert_eq!(map.get((1, 1).into()).unwrap(), &Some(3));
     }
 }
